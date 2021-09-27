e1d3a58ff5c6cd7353160428e7b9f3ed6356122d
gabrielkheisa <gabrielkheisa@gmail.com>
2021-09-27 18:13:39 +0700

    rename tabel

--- index.php
+++ index.php
@@ -195,8 +195,8 @@
               <div class="card-header ">
                 <div class="row">
                   <div class="col-sm-6 text-left">
-                    <h5 class="card-category">Total Shipments</h5>
-                    <h2 class="card-title">Performance</h2>
+                    <h5 class="card-category">Temperature</h5>
+                    <h2 class="card-title">in degree celcius</h2>
                   </div>
                   <div class="col-sm-6">
                     <div class="btn-group btn-group-toggle float-right" data-toggle="buttons">
@@ -237,7 +237,7 @@
           <div class="col-lg-4">
             <div class="card card-chart">
               <div class="card-header">
-                <h5 class="card-category">Total Shipments</h5>
+                <h5 class="card-category">Humidity</h5>
                 <h3 class="card-title"><i class="tim-icons icon-bell-55 text-primary"></i> 763,215</h3>
               </div>
               <div class="card-body">
