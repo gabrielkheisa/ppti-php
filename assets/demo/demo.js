a91f7bcedcc2ea426b1652d2e885b5beacc72588
gabrielkheisa <gabrielkheisa@gmail.com>
2021-11-05 02:52:34 +0700

    hapus chart kelembapan

--- assets/demo/demo.js
+++ assets/demo/demo.js
@@ -349,35 +349,7 @@ demo = {
       }
     };
 
-    //var ctx = document.getElementById("chartLinePurple").getContext("2d");
 
-    var gradientStroke = ctx.createLinearGradient(0, 230, 0, 50);
-
-    gradientStroke.addColorStop(1, 'rgba(72,72,176,0.2)');
-    gradientStroke.addColorStop(0.2, 'rgba(72,72,176,0.0)');
-    gradientStroke.addColorStop(0, 'rgba(119,52,169,0)'); //purple colors
-
-    //KONFIGURASI CHART KELEMBAPAN
-    var data = {
-      labels: ['0', '1', '2', '3', '4', '5'],
-      datasets: [{
-        label: "Kelembapan",
-        fill: true,
-        backgroundColor: gradientStroke,
-        borderColor: '#d048b6',
-        borderWidth: 2,
-        borderDash: [],
-        borderDashOffset: 0.0,
-        pointBackgroundColor: '#d048b6',
-        pointBorderColor: 'rgba(255,255,255,0)',
-        pointHoverBackgroundColor: '#d048b6',
-        pointBorderWidth: 20,
-        pointHoverRadius: 4,
-        pointHoverBorderWidth: 15,
-        pointRadius: 4,
-        data: [0,0,0,0,0,0],
-      }]
-    };
 
     var myChart = new Chart(ctx, {
       type: 'line',
