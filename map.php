e08bad5b0d4f8f6f90cb108afa9ae98adebe0bca
gabrielkheisa <gabrielkheisa@gmail.com>
2021-11-04 17:24:00 +0700

    ganti parsefloat ke number di add_map_point

--- map.php
+++ map.php
@@ -62,7 +62,7 @@
       var vectorLayer = new ol.layer.Vector({
         source:new ol.source.Vector({
           features: [new ol.Feature({
-                geometry: new ol.geom.Point(ol.proj.transform([parseFloat(lng), parseFloat(lat)], 'EPSG:4326', 'EPSG:3857')),
+                geometry: new ol.geom.Point(ol.proj.transform([Number(lng), Number(lat)], 'EPSG:4326', 'EPSG:3857')),
             })]
         }),
         style: new ol.style.Style({
